44cbc37ce6f45692834e2e0181ab9ab8d096a1ad
lray138 <57813289+lray138@users.noreply.github.com>
2026-02-09 18:29:15 -0500

    Adding compose

--- src/functions.php
+++ src/functions.php
@@ -6,3 +6,10 @@ function identity($value) {
     return $value;
 }
 
+function compose(callable ...$fns): callable {
+    return array_reduce(
+        array_reverse($fns),
+        fn($acc, $fn) => fn($x) => $fn($acc($x)),
+        'lray138\\Fp\\identity'
+    );
+}
